a8c219a34ecce424f60b4abf54be93d8830a3490
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-16 15:33:17 -0300

    :sparkles: fetilizer-entity

---
+++ src/main/java/com/betrybe/agrix/models/entities/Fertilizer.java
@@ -0,0 +1,73 @@
+package com.betrybe.agrix.models.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ * Entidade fertilizer.
+ */
+@Entity
+@Table(name = "fertilizer")
+public class Fertilizer {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private String brand;
+
+  private String composition;
+
+  /**
+   * Constructor vazio.
+   */
+  public Fertilizer() {
+  }
+
+  /**
+   * Constructor preenchido.
+   */
+  public Fertilizer(Long id, String name, String brand, String composition) {
+    this.id = id;
+    this.name = name;
+    this.brand = brand;
+    this.composition = composition;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public String getComposition() {
+    return composition;
+  }
+
+  public void setComposition(String composition) {
+    this.composition = composition;
+  }
+}
