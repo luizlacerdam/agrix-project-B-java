cfac6cd4ee21585d5c4ae5963a5c419fd3591ab8
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-16 15:34:00 -0300

    :sparkles: fetilizer-repository

---
+++ src/main/java/com/betrybe/agrix/models/repositories/FertilizerRepository.java
@@ -0,0 +1,8 @@
+package com.betrybe.agrix.models.repositories;
+
+import com.betrybe.agrix.models.entities.Fertilizer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FertilizerRepository extends JpaRepository<Fertilizer, Long> {
+
+}
