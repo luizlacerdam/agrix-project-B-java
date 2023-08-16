3aa59e06e3ab18b6ef18ee76cc35785b4cc1ea80
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-16 15:41:01 -0300

    :sparkles: FetilizerDto

---
+++ src/main/java/com/betrybe/agrix/controllers/dto/FertilizerDto.java
@@ -0,0 +1,14 @@
+package com.betrybe.agrix.controllers.dto;
+
+import com.betrybe.agrix.models.entities.Fertilizer;
+
+/**
+ * Fertilizer DTO.
+ */
+public record FertilizerDto(Long id, String name, String brand, String composition) {
+
+  public Fertilizer toFertilizer() {
+    return new Fertilizer(id, name, brand, composition);
+  }
+
+}
