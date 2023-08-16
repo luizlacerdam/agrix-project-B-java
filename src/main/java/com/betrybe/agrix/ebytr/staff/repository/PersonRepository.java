0bc8a36d2069ad020b49f9566b46378ac3a94937
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-16 07:55:23 -0300

    :sparkles: [feat] migração-faseA-para-faseB

--- src/main/java/com/betrybe/agrix/ebytr/staff/repository/PersonRepository.java
+++ src/main/java/com/betrybe/agrix/ebytr/staff/repository/PersonRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 /**
  * Person JPA repository.
  */
-@Repository
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
   Optional<Person> findByUsername(String username);
