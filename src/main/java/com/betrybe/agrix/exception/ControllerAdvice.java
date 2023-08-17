6d1ee49378c1052fa8383b0a34944f855aa51dd0
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-17 11:41:57 -0300

    :sparkles: handleNotFound

---
+++ src/main/java/com/betrybe/agrix/exception/ControllerAdvice.java
@@ -0,0 +1,12 @@
+package com.betrybe.agrix.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class ControllerAdvice {
+  @ExceptionHandler(NotFoundError.class)
+  public ResponseEntity<String> handleNotFound(NotFoundError error) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.getMessage());
+  }
+}
