8ce8e7e272d00913c7539ae001c66e3050324a51
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-17 20:59:22 -0300

    :sparkles: tentativa-req-2

--- src/test/java/com/betrybe/agrix/solution/PersonServiceTest.java
+++ src/test/java/com/betrybe/agrix/solution/PersonServiceTest.java
@@ -23,6 +23,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 public class PersonServiceTest {
 
